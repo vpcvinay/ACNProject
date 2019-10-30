871e302087ba2700a551b49a21923f1604cabf6a
Pooja <poojabn1617@gmail.com>
2019-10-30 16:37:44 -0500

    project1 phase 4

--- clientB.py
+++ clientB.py
@@ -1,55 +1,65 @@
-import time, socket,thread,sys
+import time, socket,threading,sys
 
 
 print("\nWelcome to Chat Room\n")
 print("Initialising....\n")
 time.sleep(1)
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-start = time.time()
-server_port=[6000,6001] #server port
 
-def fog_node_connect(server_port):
-	ip = "127.0.0.1"
-	client_port = 6001 #own port
-	conn_establ = False
-	s.bind((ip,client_port))
-	while True:
-		try:
-			s.settimeout(2)
-			s.listen(1)
-			conn, addr = s.accept()
-			if conn:
-				print("Connection established with client with port : {}".format(client_port))
-				conn_establ = True
-				break
-		except:
-			print("Connection not established")
-			s.shutdown(1)
-			s.close()
-			break
-	if not conn_establ:	
+class fog_node:
+	def __init__(self):
+		self.received_conn = []
+		self.no_of_conn = 0
+		self.server_port=[1234,6000] #server port
+		self.neighbors = len(self.server_port)
+	
+	def conn_establish(self):
+		ip = "127.0.0.1"
 		s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		s.connect(('127.0.0.1', server_port))
-	
-		print( "(", ip, ")\n")
-		name = input(str("\nEnter your name: "))
-	
-		print("\nTrying to connect to ", "(", server_port, ")\n")
-		time.sleep(1)
-	
-		print("Connected...\n")
-	
-		while 1:
-			message = input(str("Please enter your message: "))
-			message = message.encode()
-			s.send(message)
-			print("Sent")
-			print("")
-			message = s.recv(1024)
-			message = message.decode()
-			print(name, ":" ,message)
-			print("")
+		start = time.time()
+		client_port = 6001 #own port
+		count = 0
+		s.bind((ip,client_port))
+		while count<self.neighbors:
+			try:
+				s.settimeout(6)
+				s.listen(3)
+				conn, addr = s.accept()
+				if conn:
+					print("Connection established with client with port : {}".format(addr[1]))
+					count+=1
+					self.received_conn.append(addr)
+					self.no_of_conn+=1
+					continue
+			except:
+				print("Connection not established")
+				s.shutdown(1)
+				count+=1
+				s.close()
+				continue
+		print("Number of connection established : {}".format(self.no_of_conn))
+		while True:
+			if self.no_of_conn==self.neighbors:
+				print("connected to all neighbors")
+				break
+			for server in self.server_port:
+				if server not in [port[1] for port in self.received_conn]:	
+					s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+					#print("\nTrying to connect to ", "(", server, ")\n")	
+					try:
+						s.settimeout(2)
+						s.connect(('127.0.0.1', server))	
+						print( "(", server, ")\n")
+						time.sleep(1)
+						print("Connected...\n")
+						self.no_of_conn+=1
+						self.received_conn.append((ip,server))
+					except:
+						#print("retrying connection to port : {}".format(server))
+						continue
+				
+fognode = fog_node()
+#thread1 = threading.Thread(target=fognode.conn_establish)
+#thread1.start()
+fognode.conn_establish()
 
-
-thread.start_new_thread(fog_node_connect,(server_port[1]))
